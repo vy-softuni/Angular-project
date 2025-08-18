38f699c391d71344437ac5a6b0a48acedd574701
vy-softuni <shark_invest@abv.bg>
2025-08-18 23:23:09 +0300

    Frontend more basics

---
+++ TrailMix/client/src/main.ts
@@ -0,0 +1,15 @@
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { errorInterceptor } from './app/core/interceptors/error.interceptor';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))
+  ]
+}).catch(err => console.error(err));
